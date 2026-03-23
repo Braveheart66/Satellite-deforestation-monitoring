c1474b12d5f0a480116ec3aed0b7be87b96a9250
Shivam Singh <shivamsinghraghuvanshi1234@gmail.com>
2026-03-23 20:55:14 +0530

    Fix Leaflet icon initialization for Vercel - use CDN URLs

--- frontend/components/MapAOIClient.tsx
+++ frontend/components/MapAOIClient.tsx
@@ -6,6 +6,7 @@ import "leaflet/dist/leaflet.css";
 import "leaflet-draw/dist/leaflet.draw.css";
 import L from "leaflet";
 import { LatLngExpression } from "leaflet";
+import { useEffect } from "react";
 
 type Props = {
   onAOISelect: (coords: number[][][]) => void;
@@ -15,6 +16,20 @@ type Props = {
 const mapCenter: LatLngExpression = [26.9124, 75.7873];
 
 export default function MapAOIClient({ onAOISelect }: Props) {
+  // Fix Leaflet icon URLs for Next.js/production
+  useEffect(() => {
+    // @ts-ignore
+    delete (L.Icon.Default.prototype as any)._getIconUrl;
+    L.Icon.Default.mergeOptions({
+      iconRetinaUrl:
+        "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon-2x.png",
+      iconUrl:
+        "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon.png",
+      shadowUrl:
+        "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png",
+    });
+  }, []);
+
   const handleCreated = (e: any) => {
     const layer = e.layer as L.Polygon;
     const latLngs = layer.getLatLngs()[0] as L.LatLng[];
